37e5964c1c39f76bcbb1e2ec95feb8cd903f4698
Anurag Jha <abhishekjha102622@gmail.com>
2025-06-22 00:23:55 +0530

    lorin room and ejs structure

---
+++ public/javascripts/script.js
@@ -0,0 +1,23 @@
+//selecting queries
+const createBtn = document.querySelector("#createTab");
+const joinBtn = document.querySelector("#joinTab");
+const createRoom = document.querySelector("#createRoomForm");
+const createJoin = document.querySelector("#joinRoomForm");
+//adding eventLinstner
+//appear joinRoom and diappering create room
+joinBtn.addEventListener("click",function(){
+    createJoin.style.display = "flex";
+    createRoom.style.display= "none";
+    createBtn.style.borderBottom = "none";
+    joinBtn.style.borderBottom = "3px solid  #5bbbff";
+    joinBtn.style.color=" #5bbbff";
+    createBtn.style.color=" #5b74a9";
+})
+createBtn.addEventListener("click",function(){
+    createJoin.style.display = "none";
+    createRoom.style.display= "flex";
+    createBtn.style.borderBottom = "3px solid  #5bbbff";
+    joinBtn.style.borderBottom = "none";
+    createBtn.style.color=" #5bbbff";
+    joinBtn.style.color=" #5b74a9";
+})
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 8000;
+//setting ejs
+app.use(express.static(path.join(__dirname,'public')));
+app.set('view engine','ejs');
+
+//routing
+app.get("/", function(req, res) {
+    res.render("chat");
+});
+
+app.get("/chat",function(req,res){
+    res.render("chat")
+})
+//listining
+app.listen(port,()=>{
+     console.log(`Server started at http://localhost:${port}`);
+})
